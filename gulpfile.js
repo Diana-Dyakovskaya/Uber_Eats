cb4ba4df0398b6f8579e0eab3dca3bce512fb93f
Diana-Dyakovskaya <diana.dyakovskaya7@gmail.com>
2021-09-04 14:37:47 +0300

    upd styles

--- gulpfile.js
+++ gulpfile.js
@@ -101,7 +101,7 @@ function startwatch() {
   // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
   watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
   // Мониторим файлы препроцессора на изменения
-  watch('app/styles/**/*.scss', styles);
+  watch(`app/styles/**/*.${preprocessor}`, styles);
   // Мониторим файлы HTML на изменения
   watch('app/**/*.html').on('change', browserSync.reload);
   // Мониторим папку-источник изображений и выполняем images(), если есть изменения
